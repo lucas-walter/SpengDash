af924f6475308f185435915842876c68c57d4a08
lucas-walter <wal18388@spengergasse.at>
2019-04-28 21:56:14 +0200

    Raumplaner (by @paulmaurovich), beginning to implement API

--- SpengDash/src/app/raumplaner/raumplaner.component.ts
+++ SpengDash/src/app/raumplaner/raumplaner.component.ts
@@ -10,6 +10,30 @@ export class RaumplanerComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    this.updateRooms();
+  }
+
+  updateRooms() {
+    var req = "http://46.101.115.220/spengdash/api/raumplaner.php";
+    var success = false;
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function () {
+      if (this.readyState == 4 && this.status == 200) {
+        var res = JSON.parse(this.responseText);
+        if (!res.lessonNumber) {
+          console.error(res);
+        }
+        else {
+          var date = new Date(res.date);
+          console.log(date);
+          document.getElementsByClassName("headerstd")[0].innerHTML = res.lessonNumber   + ". Stunde am " + date.toLocaleDateString('de-DE', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' }); 
+          document.getElementsByClassName("headerstd")[1].innerHTML = res.lessonNumber+1 + ". Stunde am " + new Date(date.setDate(date.getDate()+1)).toLocaleDateString('de-DE', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' });
+          console.log(res);
+        }
+      }
+    };
+    xmlhttp.open("GET", req, true);
+    xmlhttp.send();
   }
 
 }
