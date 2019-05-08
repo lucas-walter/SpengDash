b03d85813f05e17b25d9e026fbcabb65bbb837fc
lucas-walter <wal18388@spengergasse.at>
2019-05-08 23:06:26 +0200

    Dashboard Raumplaner und Stundenplan

--- SpengDash/src/app/dashboard/dashboard.component.ts
+++ SpengDash/src/app/dashboard/dashboard.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { RaumplanerComponent } from '../raumplaner/raumplaner.component';
 
 @Component({
   selector: 'app-dashboard',
@@ -18,11 +19,69 @@ export class DashboardComponent implements OnInit {
     this.fetchTerm();
     this.fetchHW();
     this.fetchHolidays();
+    this.fetchRooms(0);
+  }
+
+  refreshHolidays() {
+    localStorage.removeItem("holidayCache");
+    this.fetchHolidays();
+  }
+
+  /**
+   * Zeigt freie Räume im Dashboard an
+   * @param currOrNext 0 wenn es diese Stunde ist, 1 wenn es die nächste ist
+   */
+  fetchRooms(currOrNext) {
+    var req = "http://46.101.115.220/spengdash/api/raumplaner.php";
+    var success = false;
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function () {
+      if (this.readyState == 4 && this.status == 200) {
+        var res = JSON.parse(this.responseText);
+        if (!res.lessonNumber) {
+          console.error(res);
+        }
+        else {
+          var date = new Date(res.date);
+          if (currOrNext == 0) {
+            document.getElementById("rlheader").innerHTML = res.lessonNumber   + ". Stunde am " + date.toLocaleDateString('de-DE', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' }); 
+            document.getElementById("rl").innerHTML = RaumplanerComponent.arrayToList(res.current.a);
+            document.getElementById("rl").innerHTML += RaumplanerComponent.arrayToList(res.current.b);
+            document.getElementById("rl").innerHTML += RaumplanerComponent.arrayToList(res.current.c);
+            $("#rl-currBtn").hide();
+            $("#rl-nextBtn").show();
+          }
+          else {
+            document.getElementById("rlheader").innerHTML = res.lessonNumber+1 + ". Stunde am " + date.toLocaleDateString('de-DE', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' }); 
+            document.getElementById("rl").innerHTML = RaumplanerComponent.arrayToList(res.next.a);
+            document.getElementById("rl").innerHTML += RaumplanerComponent.arrayToList(res.next.b);
+            document.getElementById("rl").innerHTML += RaumplanerComponent.arrayToList(res.next.c);
+            $("#rl-currBtn").show();
+            $("#rl-nextBtn").hide();
+          }
+        }
+      }
+    };
+    xmlhttp.open("GET", req, true);
+    xmlhttp.send();
   }
 
   fetchHolidays() {
     if (localStorage.getItem("holidayCache")) {
-      DashboardComponent.addTableRows(localStorage.getItem("holidayCache"), $("#freiList")); // Use cached holiday values
+      // Use cached holiday values
+      $("#freiList").html(""); // Clear the table's contents
+      var res = JSON.parse(localStorage.getItem("holidayCache"));
+      for (var i in res) {
+        if (DashboardComponent.parseUntisDate(res[i].startDate)) {
+          $("#freiList").append(
+            "<tr>" +
+            `<td scope="row">${res[i].longName}</td>` +
+            `<td>${DashboardComponent.parseUntisDate(res[i].startDate)}</td>` +
+            `<td>${DashboardComponent.parseUntisDate(res[i].endDate)}</td>` +
+            "</tr>"
+          );
+        }
+      }
     }
     var req = "http://46.101.115.220/spengdash/api/freieTage.php";
     var success = false;
@@ -31,7 +90,22 @@ export class DashboardComponent implements OnInit {
       if (this.readyState == 4 && this.status == 200) {
         var res = JSON.parse(this.responseText);
         console.log(res);
-        DashboardComponent.addTableRows(res, $("#freiList"));
+        res.sort((a, b) => (a.startDate > b.startDate) ? 1 : ((b.startDate > a.startDate) ? -1 : 0));
+        console.log(res);
+        // Add rows
+        $("#freiList").html(""); // Clear the table's contents
+        for (var i in res) {
+          if (DashboardComponent.parseUntisDate(res[i].startDate)) {
+            $("#freiList").append(
+              "<tr>" +
+              `<td scope="row">${res[i].longName}</td>` +
+              `<td>${DashboardComponent.parseUntisDate(res[i].startDate)}</td>` +
+              `<td>${DashboardComponent.parseUntisDate(res[i].endDate)}</td>` +
+              "</tr>"
+            );
+          }
+        }
+
         localStorage.setItem("holidayCache", this.responseText);
         console.log(res);
       }
@@ -39,6 +113,13 @@ export class DashboardComponent implements OnInit {
     xmlhttp.open("GET", req, true);
     xmlhttp.setRequestHeader("SPD-TOKEN", localStorage.getItem("user_token"));
     xmlhttp.send();
+  }
+
+  static parseUntisDate(date) {
+    var m = /\d{2}(\d{2})(\d{2})(\d{2})/gi.exec(date);
+    if (Number(m[2]) >= new Date().getMonth() + 1 && Number(m[1]) >= Number(String(new Date().getFullYear()).substr(2)))
+      return m[3] + "." + m[2] + "." + m[1];
+    else return null;
   }
 
   fetchTerm() {
