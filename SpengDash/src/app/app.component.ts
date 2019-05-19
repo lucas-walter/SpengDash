012053db05d545154b812c5dee6fb11c165c374d
lucas-walter <lucaswalter@gmx.at>
2019-05-19 23:18:42 +0200

    News

--- SpengDash/src/app/app.component.ts
+++ SpengDash/src/app/app.component.ts
@@ -29,5 +29,19 @@ export class AppComponent {
       this.titleService.setTitle( "SpengDash STAGING" );
       $('#header-logo').fadeTo(5,0.5)
     }
+
+    $.get( "http://46.101.115.220/spengdash/api/news.php", function( data ) {
+      console.log(data.data[0]);
+      if (data.data[0].id == localStorage.getItem("closedNews")) return;
+      $("#news").html(
+        '<a href="#" class="close" data-dismiss="alert" aria-label="close" id="newsDismiss">×</a>' + 
+        "<h3>" + data.data[0].title + "</h3>" + 
+        "<span>" + data.data[0].details + "</span>"
+      );
+      $("#newsDismiss").click(function () {
+        localStorage.setItem("closedNews", data.data[0].id)
+      })
+      $("#news").show();
+    });
   }
 }
